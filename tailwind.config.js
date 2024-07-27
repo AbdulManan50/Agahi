dfe4d7faa2d94423cc6b8be3dda8c4e2ad8cc113
AbdulManan50 <mananiqbal10@gmail.com>
2024-07-27 15:08:55 +0500

    responsive ness

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,27 +4,32 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        poppines: ["Poppins, sans-serif"],
+        poppins: ["Poppins, sans-serif"],
+      },
+      animation: {
+        kenburns: "kenburns 20s ease-in-out infinite",
+      },
+      keyframes: {
+        kenburns: {
+          "0%": {
+            transform: "scale(1) translate(0, 0)",
+          },
+          "50%": {
+            transform: "scale(1.1) translate(5%, 5%)",
+          },
+          "100%": {
+            transform: "scale(1) translate(0, 0)",
+          },
+        },
       },
     },
     fontFamily: {
       Merriweather: ["Merriweather"],
     },
-    animation: {
-      kenburns: 'kenburns 20s ease-in-out infinite',
-    },
-    keyframes: {
-      kenburns: {
-        '0%': {
-          background: ' scale(1) translate(0, 0)', 
-        },
-        '50%': {
-          background: 'scale(1.1) translate(5%, 5%)', 
-        },
-        '100%': {
-          background: 'scale(1) translate(0, 0)',
-        },
-      },
+    screens: {
+      sm: "426px",
+      md: "769px",
+      xl: "1280px",
     },
   },
   plugins: [],
